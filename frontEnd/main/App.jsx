e1694a815dcb7c40e189d9f2a8a7e514490d313d
Benjamin Polancich <polancichben@gmail.com>
2020-06-17 17:30:43 -0500

    initial file structure

---
+++ frontEnd/main/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Axios from 'axios';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+
+    }
+  }
+
+
+
+
+
+
+  render(){
+    return (
+      <div>
+        Test
+      </div>
+    )
+  }
+}
+
+export default App;
