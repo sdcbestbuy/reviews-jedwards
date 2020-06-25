7d4273abd04a6eabd3cb2ab6e6c83140f4501c16
Benjamin Polancich <polancichben@gmail.com>
2020-06-24 20:11:12 -0500

    server compile

---
+++ server/server.compiled.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+
+var path = require('path');
+
+var PORT = process.env.PORT;
+
+var dbquery = require('./db/queries');
+
+var _require = require('http-proxy-middleware'),
+    createProxyMiddleware = _require.createProxyMiddleware; // REVIEWS COMPONENT
+
+
+app.use(express["static"](path.join(__dirname, '../frontEnd/dist')));
+app.use(express.json());
+app.get('/api/getReviews', function (req, res) {
+  console.log('Got a get req for id', req.query.id);
+  dbquery.getTheReviews(req.query.id, function (error, results) {
+    if (error) {
+      console.log('ERROR IN SERVER getTheReviews', error);
+      res.status(400).send(error);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+}); // Need to move server compiled into same folder as server
+
+app.listen(PORT, function () {
+  console.log("server is CONNECTED on PORT:".concat(PORT));
+});
