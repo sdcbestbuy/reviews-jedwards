17fa7303bf27d0588c85b5018d41656b3acbcf65
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-08 14:07:30 -0500

    added endpoint skeletons

--- newServer/server.js
+++ newServer/server.js
@@ -9,6 +9,20 @@ const PORT = 8008;
 app.use(express.static(path.join(__dirname, '../frontEnd/dist')));
 app.use(express.json());
 
+app.get('/getReviews', (error, result) => {
+    
+})
+app.post('/getReviews', (error, result) => {
+
+})
+
+app.update('/getReviews', (error, result) => {
+    
+})
+app.delete('/getReviews', (error, result) => {
+
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
