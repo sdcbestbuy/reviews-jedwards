5e893a8d544d38b414eb9f199f7082589135b474
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-08 14:08:18 -0500

    updated end points

--- newServer/server.js
+++ newServer/server.js
@@ -9,19 +9,42 @@ const PORT = 8008;
 app.use(express.static(path.join(__dirname, '../frontEnd/dist')));
 app.use(express.json());
 
-app.get('/getReviews', (error, result) => {
-    
-})
-app.post('/getReviews', (error, result) => {
+app.get('/getReviews', ((error, result) => {
+    if(error) {
+        console.error(error);
+    } else {
 
-})
+        console.log(result);
+    }
+}));
 
-app.update('/getReviews', (error, result) => {
-    
-})
-app.delete('/getReviews', (error, result) => {
+app.post('/getReviews', ((error, result) => {
+    if(error) {
+        console.error(error);
+    } else {
 
-})
+        console.log(result);
+    }
+}));
+
+app.update('/getReviews', ((error, result) => {
+    if(error) {
+        console.error(error);
+    } else {
+
+        console.log(result);
+    }
+}));
+
+app.delete('/getReviews', ((error, result) => {
+    if(error) {
+        console.error(error);
+    } else {
+
+        console.log(result);
+    }
+}));
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
